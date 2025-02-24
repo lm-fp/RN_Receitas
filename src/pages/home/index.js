0ca25facb36f36ec87d104940b439bcf34855c94
Leticia Maria <lm.pires0.16@gmail.com>
2025-02-23 22:06:48 -0300

    commit

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Pagina home aqui!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
